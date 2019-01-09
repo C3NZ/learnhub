de34e53933d1bd4da5c5e771b04131b4d27905f4
C3NZ <cenz@cenz.io>
2019-01-09 01:02:32 -0800

    [feat] add routes for future populated pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', res.locals);
+});
+
+/* GET popular page. */
+router.get('/popular', (req, res, next) => {
+    res.render('popular', res.locals);
+});
+
+/* GET subjects page. */
+router.get('/subjects', (req, res, next) => {
+    res.render('subjects', res.locals);
+});
+
+/* GET math page page. */
+router.get('/subjects/:subjectName', (req, res, next) => {
+    res.locals.subjectName = req.params.subjectName; 
+    res.render('subject', res.locals);
 });
 
 module.exports = router;
