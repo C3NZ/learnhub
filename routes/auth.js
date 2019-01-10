7da5c3e5a5db3a8c7a49f56d0ddf694c3be2a8e0
C3NZ <cenz@cenz.io>
2019-01-10 02:00:31 -0800

    [feat] add styling for auth related pages and fix auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,72 @@ const router = express.Router();
 // Get the sign up form for the user
 router.get('/signup', (req, res) => {
     res.render('signup', res.locals);
-})
+});
 
 // Get the login form for the user
 router.get('/login', (req, res) => {
     res.render('login', res.locals)
 });
 
+// Create a new login
+router.post('/', (req, res, next) => {
+    User
+        .create(req.body)
+        .then((user) => {
+            const tokenPayload = {
+                username: user.username,
+                id: user._id,
+            }
+
+            const tokenHeaders = { expiresIn: '60 days' };
+            const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, tokenHeaders);
+
+            // Create a cooke with the encrypted jwt token, then send it to the user
+            res.cookie('lhAuth', token, { maxAge: 60 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.render('profile', res.locals);
+        })
+        .catch((err) => {
+            return next(err);
+        })
+});
+
+// Sign the user in
+router.put('/', (req, res) => {
+    const { username, password } = req.body;
+
+    User
+        .findOne({ username }, 'username password')
+        .then((user) => {
+
+            // Render the login page with an error
+            if (!user) {
+                return res.status(403).render('login', res.locals);
+            }
+
+            user.comparePassword(password, (err, isMatch) => {
+                if (isMatch) {
+                    const tokenPayload = {
+                        username: user.username,
+                        id: user._id,
+                    };
+
+                    const tokenHeaders = { expiresIn: '60 days' };
+                    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, tokenHeaders);
+
+                    // Create a cookie with a encrypted jwt token, then send it to the user
+                    res.cookie('lhAuth', token, { maxAge: 60 * 24 * 60 * 60 * 1000, httpOnly: true });
+                    return res.render('profile', res.locals);
+                }
+                return res.status(403).render('login', res.locals);
+            });
+        })
+        .catch(err => res.status(500).render('login', res.locals));
+});
+
+// Sign the user out
+router.delete('/', (req, res) => {
+    res.clearCookie('lhAuth');
+    res.render('login', res.locals);
+});
+
+module.exports = router;
